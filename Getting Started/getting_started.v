3626e16436202f8c8fed98a90327727758913406
Seunghyeok Lee <87926645+shstevelee@users.noreply.github.com>
2026-02-03 01:31:39 +0000

    update: getting started

---
+++ Getting Started/getting_started.v
@@ -0,0 +1,8 @@
+// Solution by Seunghyeok Lee
+// Problem: Getting Started
+
+module top_module( output one );
+
+    assign one = 1'b1;
+    
+endmodule
---
+++ Getting Started/zero.v
@@ -0,0 +1,5 @@
+module top_module ( ouput zero ) 
+    
+    assign zero = 1'b0;
+
+endmodule
